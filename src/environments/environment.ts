20dc0a251766a7f61b95c8b5357a94d223c014ad
Hamilton8560 <60953362+Hamilton8560@users.noreply.github.com>
2024-01-11 15:50:53 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,26 +1,9 @@
 export const environment = 
     
 {
-    production:false,
-    firebase:{
-        apiKey: "AIzaSyD5rWsaTAPJD343qtqLeJ4TdxvqhmmBdhg",
 
-        authDomain: "auth-project-2ed86.firebaseapp.com",
-      
-        projectId: "auth-project-2ed86",
-      
-        storageBucket: "auth-project-2ed86.appspot.com",
-      
-        messagingSenderId: "791744484218",
-      
-        appId: "1:791744484218:web:967d50bba6b6a53133fcc3",
-      
-        measurementId: "G-CZ0F84J1R2"
-      
-    }
 
 };
 
 
 export const stripe_key= 
-    "sk_test_51NDASeHv8mumYCroLPdMO7BWRmh4v5dFAsTlX5lnIBGY6urv7dqOTe6tKPCh8fs0T0oGQZsaegRKsRVX776RD8Qq00NdfRXVzt"
