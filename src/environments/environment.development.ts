f2eb5f6807e287e2327b77b87cee0ddd34527710
Hamilton8560 <60953362+Hamilton8560@users.noreply.github.com>
2024-01-11 15:51:06 -0500

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,20 +1,6 @@
 export const environment = {
 
-    production:true,
-    firebase:{
-        apiKey: "AIzaSyD5rWsaTAPJD343qtqLeJ4TdxvqhmmBdhg",
 
-        authDomain: "auth-project-2ed86.firebaseapp.com",
-      
-        projectId: "auth-project-2ed86",
-      
-        storageBucket: "auth-project-2ed86.appspot.com",
-      
-        messagingSenderId: "791744484218",
-      
-        appId: "1:791744484218:web:967d50bba6b6a53133fcc3",
-      
-        measurementId: "G-CZ0F84J1R2"
       
     }
 };
